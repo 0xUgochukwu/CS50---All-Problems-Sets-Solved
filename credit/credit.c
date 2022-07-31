69a5a903afcffa4d8eb66234b015264420d8c60d
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-07-31 14:05:27 +0000

    Sun, Jul 31, 2022, 2:05 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -10,14 +10,22 @@ int main(void)
     // declaring variables I will need
     int mod1;
     int mod2;
-    int sum1;
-    int sum2;
+    int sum1 = 0;
+    int sum2 = 0;
 
-    // Getting Every other digit
+    // Breaking the Card Number
     while(cardNumber)
     {
+        // Getting the last digit and other digits
         mod1 = cardNumber % 10;
+        cardNumber /= 10; // reducing the number
+        sum1 = sum1 + mod1;
+
+        // Getting every other digit in the card and
+        mod2 = cardNumber % 10;
         cardNumber /= 10;
+        sum2 = sum2 + (mod2 * 2);
+
 
     }
 }
