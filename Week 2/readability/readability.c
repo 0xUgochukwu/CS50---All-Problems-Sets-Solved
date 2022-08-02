5bb7569350fb9b06162f9836eb6d08198f3e3050
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-02 08:42:20 +0000

    Tue, Aug 2, 2022, 8:42 AM +00:00

--- Week 2/readability/readability.c
+++ Week 2/readability/readability.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string text)
+
 int main(void)
 {
-
+    string text = get_string("Text: ");
+    int n = count_letters(text);
+    printf("%i", n);
 }
 
 int count_letters(string text)
@@ -12,6 +16,11 @@ int count_letters(string text)
     int letters = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if
+        if (text[i] != 32)
+        {
+            letters ++;
+        }
     }
+
+    return letters;
 }
