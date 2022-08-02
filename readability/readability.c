c50bd2e375ac04195748c8011affe6178e7e611f
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-02 10:27:47 +0000

    Tue, Aug 2, 2022, 10:27 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -25,10 +25,13 @@ int main(void)
     if (index < 1)
     {
         printf("Before Grade 1\n");
-    } else if (index >= 16)
+    }\n
+    else if (index >= 16)
     {
         printf("Grade 16+\n");
-    } else {
+    }\n
+    else\n
+    {
         printf("Grade %i\n", index);
     }
 }
