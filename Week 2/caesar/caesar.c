4f7648c8dfcf373be14c553454088197a89e35ee
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-02 11:28:39 +0000

    Tue, Aug 2, 2022, 11:28 AM +00:00

--- Week 2/caesar/caesar.c
+++ Week 2/caesar/caesar.c
@@ -1,11 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("error: No Argument passed or The Argument you passed is not Valid\n");
+        printf("Usage: ./caesar key\n");
         return 1;
+    }
+    else
+    {
+
+    }
+}
+
+int only_digits(string number)
+{
+    bool is_only_didits = true;
+    int n = strlen(number);
+
+
+    for (int i = 0; i < n; i++)
+    {
+        
     }
 }
