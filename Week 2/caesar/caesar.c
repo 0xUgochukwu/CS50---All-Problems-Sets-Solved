00b1f167f3051a3a48b3da2f7dd08d0f3378bf17
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-02 11:21:30 +0000

    Tue, Aug 2, 2022, 11:21 AM +00:00

--- Week 2/caesar/caesar.c
+++ Week 2/caesar/caesar.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    if (argc != 2)
+    {
+        printf("error: No Argument passed or The Argument you passed is not Valid\n");
+        return 1;
+    }
 }
