79b77b1484d0e4118583f8f203cb8a924d341113
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-02 10:25:39 +0000

    Tue, Aug 2, 2022, 10:25 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -19,7 +19,7 @@ int main(void)
     float S = (sentences / (float) words) * 100;
 
 
-    float index = 0.0588 * L - 0.296 * S - 15.8;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
 
 
     if (index < 1)
@@ -29,7 +29,7 @@ int main(void)
     {
         printf("Grade 16+\n");
     } else {
-        printf("Grade %i\n", (int) round(index));
+        printf("Grade %i\n", index);
     }
 }
 
