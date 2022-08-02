a1bd228ad782b3b52abf8ca6afd1f8644d823147
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-02 08:45:40 +0000

    Tue, Aug 2, 2022, 8:45 AM +00:00

--- Week 2/readability/readability.c
+++ Week 2/readability/readability.c
@@ -16,7 +16,7 @@ int count_letters(string text)
     int letters = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if (text[i] != 32)
+        if (text[i] > 97 && text[i] < )
         {
             letters ++;
         }
