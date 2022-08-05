1097ec87d487b22a4c2d5e49dafd848fdd8bba84
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-05 21:19:50 +0000

    Fri, Aug 5, 2022, 9:19 PM +00:00

--- Week 3/runoff/runoff.c
+++ Week 3/runoff/runoff.c
@@ -190,9 +190,12 @@ bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        if (!candidates[i].eliminated && candidates[i].votes != min)
+        {
+            return false;
+        }
     }
-    return false;
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
