d6a7141526268a2d797b59dd71017607c198c651
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-05 21:09:38 +0000

    Fri, Aug 5, 2022, 9:09 PM +00:00

--- Week 3/runoff/runoff.c
+++ Week 3/runoff/runoff.c
@@ -172,8 +172,16 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min_votes = 0;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes < min_votes)
+        {
+            min_votes = candidates[i].votes;
+        }
+    }
+    return  min_votes;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
