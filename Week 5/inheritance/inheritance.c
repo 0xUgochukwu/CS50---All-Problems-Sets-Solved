97d4781ab615a001c9b6e1d1a6cc00d9325fe67b
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-11 13:27:27 +0000

    ddd, Aug 8/11/2022, YYYY, 1:27 A +0

--- Week 5/inheritance/inheritance.c
+++ Week 5/inheritance/inheritance.c
@@ -69,22 +69,31 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        
+        current_person.parents[0] = NULL;
+        current_person.parents[1] = NULL;
 
         // TODO: Randomly assign alleles
+
+        current_person.alleles[0] = random_allele();
+        current_person.alleles[1] = random_allele();
 
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return current_person;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
+    free(p);
 
     // TODO: Free parents recursively
+    if (p.parents[0] != NULL)
+    {
+        free
+    }
 
     // TODO: Free child
 
