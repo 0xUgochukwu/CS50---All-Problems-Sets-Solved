1b7d292001b7cb61fa066ddf4fde9b5c62ef6a8f
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-14 14:52:13 +0000

    Sun, Aug 14, 2022, 2:52 PM +00:00

--- Week 6/sentimental-cash/cash.py
+++ Week 6/sentimental-cash/cash.py
@@ -1,1 +1,33 @@
 # TODO
+from cs50 import get_float
+
+# Initialize coins
+
+coins = 0
+
+# Prompt user for the amount
+
+while True:
+    amount = round(int(get_float("How many cents? ")))
+    if amount > 0:
+        break
+
+
+# find out how many coins
+
+while amount > 0:
+    while amount >= 25:
+        coins += 1
+        amount -= 25
+    while amount >= 10:
+        coins += 1
+        amount -= 10
+    while amount >= 5:
+        coins += 1
+        amount -= 25
+    while amount >= 1:
+        coins += 1
+        amount -= 1
+
+
+print(coins)
