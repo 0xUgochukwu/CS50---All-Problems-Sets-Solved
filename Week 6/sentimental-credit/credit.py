44e234c6f74b098f26cb55a6a5319c1c96ea399a
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-15 08:07:17 +0000

    Mon, Aug 15, 2022, 8:07 AM +00:00

--- Week 6/sentimental-credit/credit.py
+++ Week 6/sentimental-credit/credit.py
@@ -4,4 +4,19 @@ from cs50 import get_int
 
 cardNumber = get_int("Number: ")
 
-while 8
+cardNumber = x
+sum1 = 0
+sum2 = 0
+
+while True:
+    mod1 = x % 10
+    x /= 10
+    sum1 += mod1
+
+    mod2 = x % 10
+    x /= 10
+
+    mod2 = mod2 * 2
+    d1 = mod2 % 10
+    d2 = mod2 /10
+
