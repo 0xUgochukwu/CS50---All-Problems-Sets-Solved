048f7adcd80656475ecc21a5d335f758c9ef3170
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-15 10:27:36 +0000

    Mon, Aug 15, 2022, 10:27 AM +00:00

--- Week 6/dna/dna.py
+++ Week 6/dna/dna.py
@@ -13,7 +13,6 @@ def main():
     # TODO: Read database file into a variable
     with open(sys.argv[1], "r") as csvfile:
         reader = csv.DictReader(csvfile)
-        print(reader)
         dict_list = list(reader)
 
 
@@ -22,7 +21,7 @@ def main():
         sequence = file.read()
 
     max_counts = []
-    
+
 
 
     # TODO: Find longest match of each STR in DNA sequence
