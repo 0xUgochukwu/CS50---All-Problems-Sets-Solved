db11096fdcabd4e2fe960c24b04fdf827d17c4fd
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-15 09:09:21 +0000

    Mon, Aug 15, 2022, 9:09 AM +00:00

--- Week 6/sentimental-readability/readability.py
+++ Week 6/sentimental-readability/readability.py
@@ -1,2 +1,26 @@
 # TODO
 
+def main():
+    text = input("Text: ")
+    letters = count_letters(text)
+    words = count_words(text)
+    sentences = count_sentences(text)
+
+    L = (letters / words) * 100
+    S = (letters / sentences) * 100
+
+    index = round(0.0588 * L - 0.296 * S - 15.8)
+
+    if index < 1:
+        print("Before Grade 1")
+    elif index >= 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {index}")
+
+
+def count_letters(text):
+    letters = 0
+
+    for i in text:
+        
