9d909b687482f47b63262d8f2c36dcb8137264d4
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-15 10:20:40 +0000

    Mon, Aug 15, 2022, 10:20 AM +00:00

--- Week 6/dna/dna.py
+++ Week 6/dna/dna.py
@@ -11,8 +11,8 @@ def main():
         sys.exit()
 
     # TODO: Read database file into a variable
-    with open(sys.argv[1], "r") as file:
-        reader = csv.DictReader(file)
+    with open(sys.argv[1], "r") as csvfile:
+        reader = csv.DictReader(csvfile)
 
 
     # TODO: Read DNA sequence file into a variable
