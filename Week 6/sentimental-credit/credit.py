622d2d020e97fba8ce72607d4624c8417e0c06da
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-15 08:54:02 +0000

    Mon, Aug 15, 2022, 8:54 AM +00:00

--- Week 6/sentimental-credit/credit.py
+++ Week 6/sentimental-credit/credit.py
@@ -2,36 +2,39 @@
 
 from cs50 import get_int
 
+# Getting the card Number from the user
 cardNumber = get_int("Number: ")
 
 x = cardNumber
 sum1 = 0
 sum2 = 0
 
+
 while True:
+    # Getting Every digit
     mod1 = x % 10
     x //= 10
     sum1 += mod1
 
+    # Getting every other digit
     mod2 = x % 10
     x //= 10
 
+    # Multiblying every other digit by two and adding each digit together
     mod2 = mod2 * 2
     d1 = mod2 % 10
     d2 = mod2 // 10
     sum2 = sum2 + d1 + d2
 
+    # Check if card numbers are exhausted
     if x <= 0:
         break
 
+# Totaling the two sums
 total = sum1 + sum2
 
-count = 0
-cc = cardNumber
-
-while cc > 0:
-    cc //= 10
-    count += 1
+# Counting how many numbers are in the card
+card
 
 if (count != 13 and count != 15 and count != 16):
     print("INVALID")
