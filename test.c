c86b8047e26a50153ff89798facfe03141ed13ee
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-18 11:33:22 +0000

    Thu, Aug 18, 2022, 11:33 AM +00:00

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	printf("Your name:\n");
+
+	// return (0);
+
+	char *name;
+	int count = 0;
+	scanf("%s", name);
+
+
+
+	printf("%lu\n", strlen(name));
+
+}
