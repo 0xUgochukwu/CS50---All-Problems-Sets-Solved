2c118478a37af5cfe16455743a5f464fc5e5221a
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-20 16:01:34 +0000

    Sat, Aug 20, 2022, 4:01 PM +00:00

--- Week 7/movies/13.sql
+++ Week 7/movies/13.sql
@@ -1,1 +1,8 @@
-SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon');
+SELECT distinct(name) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.title IN(
+SELECT distinct(movies.title) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND people.name != "Kevin Bacon";
