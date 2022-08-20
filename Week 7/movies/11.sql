ce66145691bd36fcab32044d81fcb2e05dddf842
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-20 15:22:42 +0000

    Sat, Aug 20, 2022, 3:22 PM +00:00

--- Week 7/movies/11.sql
+++ Week 7/movies/11.sql
@@ -7,5 +7,5 @@ SELECT movies.title FROM movies
 INNER JOIN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Chadwick Boseman'))
 AS chadMovies ON chadMovies.movie_id = movies.id
 INNER JOIN ratings ON ratings.movie_id = movies.id;
-ORDER BY ratings.rating
+ORDER BY ratings.rating DESC
 LIMIT 5;
