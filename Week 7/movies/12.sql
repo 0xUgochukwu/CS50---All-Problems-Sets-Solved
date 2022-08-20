493143f6b70a03ca0b65e251b6b1eb12b182bfbf
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-20 15:41:28 +0000

    Sat, Aug 20, 2022, 3:41 PM +00:00

--- Week 7/movies/12.sql
+++ Week 7/movies/12.sql
@@ -1,3 +1,8 @@
-SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Johnny Depp')
-UNION
-SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Helena Bonham');
+SELECT COUNT(movies.title) FROM movies
+INNER JOIN (
+    SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Johnny Depp')
+    UNION
+    SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Helena Bonham')
+    )
+AS newMovies ON newMovies.movie_id = movies.id
+
