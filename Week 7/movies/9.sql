2a77d89a0fa0abf86c68b5c7661bb1bd60518557
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-20 16:11:03 +0000

    Sat, Aug 20, 2022, 4:11 PM +00:00

--- Week 7/movies/9.sql
+++ Week 7/movies/9.sql
@@ -1,4 +1,5 @@
 SELECT people.name FROM stars
 INNER JOIN people ON stars.person_id = people.id
 INNER JOIN movies ON stars.movie_id = movies.id
-WHERE movies.year = 2004;
+WHERE movies.year = 2004
+ORDER BY people.birth;
