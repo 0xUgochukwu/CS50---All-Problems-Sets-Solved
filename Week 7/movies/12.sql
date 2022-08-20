f61d1b918773b901ed3d7f79c5a794a625c21abf
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-20 15:38:53 +0000

    Sat, Aug 20, 2022, 3:38 PM +00:00

--- Week 7/movies/12.sql
+++ Week 7/movies/12.sql
@@ -1,1 +1,3 @@
-SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Johnny Depp') UNION (SELECT id FROM people WHERE name = 'Helena Bonham');
+SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Johnny Depp')
+UNION
+SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Helena Bonham');
