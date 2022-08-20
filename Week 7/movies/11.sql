e4478c5ebc2a2c6f0a28c9c62b39bd1c9e3e6278
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-20 16:12:06 +0000

    Sat, Aug 20, 2022, 4:12 PM +00:00

--- Week 7/movies/11.sql
+++ Week 7/movies/11.sql
@@ -1,6 +1,7 @@
-SELECT movies.title FROM movies
-INNER JOIN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Chadwick Boseman'))
-AS chadMovies ON chadMovies.movie_id = movies.id
-INNER JOIN ratings ON ratings.movie_id = movies.id;
+SELECT movies.title FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE name = "Chadwick Boseman"
 ORDER BY ratings.rating DESC
 LIMIT 5;
