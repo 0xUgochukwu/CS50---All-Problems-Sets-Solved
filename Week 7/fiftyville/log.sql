f463ed2ef18d7cb1ed8978f826248e45b8cc810a
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-21 10:27:02 +0000

    Sun, Aug 21, 2022, 10:27 AM +00:00

--- Week 7/fiftyville/log.sql
+++ Week 7/fiftyville/log.sql
@@ -89,4 +89,4 @@ JOIN (
 ON people.phone_number = suspects.receiver
 JOIN bank_accounts ON people.id = bank_accounts.person_id
 JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
-WHERE year >= 2021 AND month >= 7 AND day >= 28;
+WHERE atm_transactions.year >= 2021 AND atm_transactions.month >= 7 AND atm_transactions.day >= 28;
