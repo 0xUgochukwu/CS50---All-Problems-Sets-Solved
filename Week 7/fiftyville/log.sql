35ad051e58625dd9c5ccc57d8321bcf0cbe5a778
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-21 08:58:27 +0000

    Sun, Aug 21, 2022, 8:58 AM +00:00

--- Week 7/fiftyville/log.sql
+++ Week 7/fiftyville/log.sql
@@ -1,3 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
-SELECT * FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7 AND hour = 10;
+-- From the desciption the crime took place at 10:15 at the bakery
+SELECT * FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7 AND hour = 10 AND  minute >= 15;
+-- If the crime took place at 10:15 then we should track all exits on and after that time
