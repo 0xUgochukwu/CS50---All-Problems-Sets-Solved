bd3c129ec8a7b88f16d9ec82b30ab6adfeec8f5d
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-21 08:55:31 +0000

    Sun, Aug 21, 2022, 8:55 AM +00:00

--- Week 7/fiftyville/log.sql
+++ Week 7/fiftyville/log.sql
@@ -1,2 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
+SELECT * FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7 AND hour = 10;
