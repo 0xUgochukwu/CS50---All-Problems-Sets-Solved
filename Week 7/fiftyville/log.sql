5284dda08d9a55757bc55232046ba3a5f1d6c80e
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-21 09:40:34 +0000

    Sun, Aug 21, 2022, 9:40 AM +00:00

--- Week 7/fiftyville/log.sql
+++ Week 7/fiftyville/log.sql
@@ -16,7 +16,7 @@ WHERE year = 2021 AND day = 28 AND month = 7 AND hour = 10 AND  minute >= 15 AND
 -- We know that shortly afterwards the thief took a flight out of town, so let's look for people that took a flight after the robery that day
 -- that are among the suspects from the result set above
 
-SELECT passengers FROM passengers
+SELECT * FROM passengers
 JOIN (
     SELECT people.id, people.name, people.phone_number, people.passport_number, people.license_plate FROM people
     JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
@@ -28,3 +28,14 @@ WHERE flights.day = 28 AND flights.month = 7 AND flights.hour >= 10 AND flights.
 
 
 -- Among the Suspects only Vanessa took a flight out of town the same day the flight left the origin ariport at 3:15PM
+-- That implies she is the thief so we find out where she went to
+SELECT * FROM airports
+WHERE id = (SELECT flights.destination_airport_id FROM passengers
+            JOIN (
+                SELECT people.id, people.name, people.phone_number, people.passport_number, people.license_plate FROM people
+                JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
+                WHERE year = 2021 AND day = 28 AND month = 7 AND hour = 10 AND  minute >= 15 AND activity = 'exit'
+                ) AS suspects
+                ON passengers.passport_number = suspects.passport_number
+            JOIN flights ON passengers.flight_id = flights.id
+            WHERE flights.day = 28 AND flights.month = 7 AND flights.hour >= 10 AND flights.minute >= 15);
