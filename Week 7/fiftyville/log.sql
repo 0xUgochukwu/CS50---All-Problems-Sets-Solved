847fa938786549f693a9e87652edb96a39833e18
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-21 09:07:11 +0000

    Sun, Aug 21, 2022, 9:07 AM +00:00

--- Week 7/fiftyville/log.sql
+++ Week 7/fiftyville/log.sql
@@ -4,3 +4,7 @@ SELECT * FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Hum
 
 SELECT * FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7 AND hour = 10 AND  minute >= 15;
 -- If the crime took place at 10:15 then we should track the first exits on or after that time as one of them is the thief
+
+SELECT people.id, people.name, people.phone_number, people.passport_number, people.license_plate FROM people
+JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
+WHERE year = 2021 AND day = 28 AND month = 7 AND hour = 10 AND  minute >= 15 AND activity = 'exit';
