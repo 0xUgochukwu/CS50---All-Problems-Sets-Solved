9bfd1e50173ad2d63f580e856aee046d68f3735b
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-08-31 10:05:00 +0000

    Wed, Aug 31, 2022, 10:05 AM +00:00

--- Week 9/birthdays/app.py
+++ Week 9/birthdays/app.py
@@ -27,7 +27,7 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        db.execute("INSERT INTO birthdays VALUES )
+        db.execute("INSERT INTO birthdays VALUES(?,?,?)", )
 
         return redirect("/")
 
